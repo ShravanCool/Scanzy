cc30908fa0850be50aef8b5c577e232de9a4d128
Shravan <shravan.akku@gmail.com>
2021-05-06 22:49:48 +0530

    refactor: modified file handling, checking file extensions

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, redirect
 from predict import predict
+import os
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
 model_path = './models/baseline.h5'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/')
 def index():
@@ -12,21 +21,36 @@ def index():
 def model_details():
     return render_template('model_details.html')
 
-@app.route('/predictor')
-def predictor():
-    return render_template('predictor.html')
+# @app.route('/predictor')
+# def predictor():
+    # return render_template('predictor.html')
 
-@app.route('/predictor', methods=['POST'])
+@app.route('/predictor', methods=['GET', 'POST'])
 def predictor():
     if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
         f = request.files['file']
 
-        basepath = os.path.dirname(__file__)
-        file_path = os.path.join(basepath, 'uploads', f.filename)
-        f.save(file_path)
+        if f.filename == '':
+            flash('No file selected')
+            return redirect(request.url)
 
-        pred = predict(file_path, model_path)
-        return render_template('predictor.html', pred=pred)
+        if f and allowed_file(f.filename):
+            # f = secure_filename(f.filename)
+            basepath = os.path.dirname(__file__)
+            file_path = os.path.join(basepath, UPLOAD_FOLDER, f.filename)
+            f.save(file_path)
+            pred = predict(file_path, model_path)
+            return render_template('predictor.html', pred=pred)
+        else:
+            flash('File Extension does not match!!')
+            return redirect(request.url)
+    else:
+        return render_template('predictor.html')
+
+
 
 @app.route('/bulletin')
 def bulletin():
@@ -34,4 +58,6 @@ def bulletin():
 
 
 if __name__ == "__main__":
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
     app.run(debug=True)
--- predict.py
+++ predict.py
@@ -1,19 +1,20 @@
-from tensorflow.keras import load_model
+from tensorflow import keras
 from keras.preprocessing import image
 import numpy as np
 
-def predict(image, model_path):
+def predict(img, model_path):
     """
     Method to take the given input image, 
     load the model, and predict if the given
     image has presence of Covid or not.
     """
+    print(image)
 
-    img = image.load_img(image, target_size = (224, 244))
-    img = img_to_array(img)
+    img = image.load_img(img, target_size = (224, 224))
+    img = image.img_to_array(img)
     img = np.expand_dims(img, axis=0)
 
-    classifier = load_model(model_path)
+    classifier = keras.models.load_model(model_path)
 
     result = classifier.predict(img)
 
